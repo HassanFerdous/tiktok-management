fdeab5ea506b15a775d30c4d3cd2c8fd02ae8879
dev-ferdous <hassanferdous101@gmail.com>
2024-10-08 12:42:52 +0600

    fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import axios from "axios";
 
 export default async function Home({ params, searchParams }: any) {
-	const res = await axios.post("https://open.tiktokapis.com/v2/oauth/token/", {
-		client_key: process.env.TIKTOK_CLIENT_ID,
-		client_secret: process.env.TIKTOK_CLIENT_SECRET,
-		code: searchParams?.code,
-		grant_type: "authorization_code",
-		redirect_uri: process.env.CLIENT_DOMAIN,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
+	const res = await axios.post(
+		"https://open.tiktokapis.com/v2/oauth/token/",
+		{
+			client_key: process.env.TIKTOK_CLIENT_ID,
+			client_secret: process.env.TIKTOK_CLIENT_SECRET,
+			code: searchParams?.code,
+			grant_type: "authorization_code",
+			redirect_uri: process.env.CLIENT_DOMAIN,
 		},
-	});
+		{
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+		}
+	);
+
+	console.log(res);
 
 	return <div className="container py-10">{JSON.stringify(res.data)}</div>;
 }
